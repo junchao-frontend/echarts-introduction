88d9d9ba8ed0de7d0c69d737152f2fea37460bb0
王军潮 <1109355524@qq.com>
2021-08-02 19:35:41 +0800

    first git

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Echarts数据可视化开始
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import * as echarts from 'echarts';
+import 'echarts-gl'
+Vue.prototype.$echarts = echarts
+
+import DataV from '@jiaminghi/data-view'
+
+Vue.use(DataV)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
